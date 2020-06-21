d1e771bb4837b807bb53b5ffd669d4f45f416dbd
kusrana <rizki.kusrana@gmail.com>
2020-06-21 23:46:19 +0700

    reverse string and some exercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,19 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    // solution 1
+    // return str.split('').reverse().join('');
+
+    // solution 2
+    // let reserved = '';
+    // for (let character of str) {
+    //     reserved = character + reserved;
+    // }
+    // return reserved;
+
+    // solution 3
+    return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
