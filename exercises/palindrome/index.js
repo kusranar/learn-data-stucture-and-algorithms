5f242221a2687b73434613661f2fdfb184a430c0
kusrana <rizki.kusrana@gmail.com>
2020-06-22 14:08:21 +0700

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,14 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    // let result = '';
+    // for (let character of str) {
+    //     result = character + result;
+    // }
+    // return result === str;
+
+    return str.split('').every((char, i) => char === str[str.length - i - 1])
+}
 
 module.exports = palindrome;
