f0e5a211122162d53421b89884d6b57293a47a6a
kusrana <rizki.kusrana@gmail.com>
2020-06-22 20:03:12 +0700

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,41 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    // let result = [];
+    // let chunked = [];
+    // let count = 0;
+    // for (let i = 0; i < array.length; i++) {
+    //     count += 1;
+    //     chunked.push(array[i]);
+    //     if (count === size || i === array.length - 1) {
+    //         count = 0;
+    //         result.push(chunked);
+    //         chunked = [];
+    //     }
+    // }
+    // return result;
+
+    // const chunked = [];
+    // for (let element of array) {
+    //     const last = chunked[chunked.length - 1];
+
+    //     if (!last || last.length === size) {
+    //         chunked.push([element]);
+    //     } else {
+    //         last.push(element);
+    //     }
+    // }
+    // return chunked;
+
+    const chunked = [];
+    let index = 0;
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
+    }
+
+    return chunked;
+}
 
 module.exports = chunk;
