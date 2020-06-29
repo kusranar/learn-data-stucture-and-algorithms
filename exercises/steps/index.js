b2c92590bb4e7635092eca7880ba973ea0022203
kusrana <rizki.kusrana@gmail.com>
2020-06-29 23:30:38 +0700

    steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,42 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+    if (n === row) return;
+
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    stair.length <= row ? stair += '#' : stair += ' ';
+
+    steps(n, row, stair);
+
+
+    // if (n === row) return;
+    // let result = '';
+    // for (let i = 0; i < n; i += 1) {
+    //     if (i <= row) {
+    //         result += '#';
+    //     } else {
+    //         result += ' ';
+    //     }
+    // }
+    // console.log(result);
+    // steps(n, row + 1);
+
+    // for (let row = 1; row <= n; row += 1) {
+    //     let result = '';
+    //     for (let column = 1; column <= n; column += 1) {
+    //         if (column <= row) {
+    //             result += '#';
+    //         } else {
+    //             result += ' ';
+    //         }
+    //     }
+    //     console.log(result);
+    // }
+}
 
 module.exports = steps;
