c1af8ca5327b29dc2b7e19feaa9b3a3360aaa699
kusrana <rizki.kusrana@gmail.com>
2020-07-18 20:55:56 +0700

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,17 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    // return str.replace(/[^aiueo]/gi, '').split('').length;
+
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+
+    // let count = 0;
+    // for (let char of str.toLowerCase()) {
+    //     if (['a', 'i', 'u', 'e', 'o'].includes(char)) count += 1;
+    // }
+    // return count;
+}
 
 module.exports = vowels;
