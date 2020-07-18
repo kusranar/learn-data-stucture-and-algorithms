51eb63fb38f35229fb6c80da806d572860bf14f1
kusrana <rizki.kusrana@gmail.com>
2020-07-18 23:15:31 +0700

    matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,49 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    const result = [];
+    for (let i = 0; i < n; i += 1) {
+        result.push([]);
+    }
+
+    let counter = 1;
+    let startCol = 0;
+    let endCol = n - 1;
+    let startRow = 0;
+    let endRow = n - 1;
+
+    while (startCol <= endCol && startRow <= endRow) {
+        for (let i = startCol; i <= endCol; i += 1) {
+            result[startRow][i] = counter;
+            counter += 1;
+        }
+
+        startRow += 1;
+
+        for (let i = startRow; i <= endRow; i += 1) {
+            result[i][endCol] = counter;
+            counter += 1;
+        }
+
+        endCol -= 1;
+
+        for (let i = endCol; i >= startCol; i -= 1) {
+            result[endRow][i] = counter;
+            counter += 1;
+        }
+
+        endRow -= 1;
+
+        for (let i = endRow; i >= startRow; i--) {
+            result[i][startCol] = counter;
+            counter += 1;
+        }
+
+        startCol += 1;
+    }
+
+    return result;
+}
 
 module.exports = matrix;
