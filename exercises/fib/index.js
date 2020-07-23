0b2bf1e640ba388ef4f0f8db25866837cd1b0229
kusrana <rizki.kusrana@gmail.com>
2020-07-23 13:23:13 +0700

    fib and momoize

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,43 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+    const cache = {};
+    return function (...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+        const result = fn.apply(this, args);
+        cache[args] = result;
+        return result;
+    }
+}
+
+function slowFib(n, nowN = 0, prevN = 1) {
+    // if (n === 0) {
+    //     return nowN;
+    // }
+
+    // return fib(n - 1, nowN + prevN, nowN);
+
+    if (n < 2) {
+        return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+
+    // const result = [0, 1];
+
+    // for (let i = 2; i <= n; i += 1) {
+    //     const a = result[i - 1];
+    //     const b = result[i - 2];
+
+    //     result.push(a + b);
+    // }
+
+    // return result[n];
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
